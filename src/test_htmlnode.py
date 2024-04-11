4f9a090c6d012b3d8f81ad1f773d164be6d57d58
clarissecasicarino <cclarissecasi@gmail.com>
2024-04-11 17:59:06 +0800

    Add leafnode child class of HTML node and its test suite

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,10 +1,10 @@
 import unittest
-
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_eq(self):
+    # HTMLNode test suite
+    def test_to_html_props(self):
         node = HTMLNode(
             "div",
             "Hello, world!",
@@ -15,6 +15,15 @@ class TestHTMLNode(unittest.TestCase):
             node.props_to_html(),
             ' class="greeting" href="https://boot.dev"',
         )
+
+    # LeafNode test suite
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 if __name__ == "__main__":
     unittest.main()
