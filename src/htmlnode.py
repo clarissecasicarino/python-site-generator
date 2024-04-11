6e22cb70f6a116fde97f0caeaea2b5502277eb51
clarissecasicarino <cclarissecasi@gmail.com>
2024-04-11 16:10:09 +0800

    Finish HTML Node and test suite

---
+++ src/htmlnode.py
@@ -0,0 +1,23 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+    
+    def __repr__(self):
+        print(f"HTMLNode tag:{self.tag}")
+        print(f"HTMLNode value:{self.value}")
+        print(f"HTMLNode children:{self.children}")
+        print(f"HTMLNode props:{self.props}")
